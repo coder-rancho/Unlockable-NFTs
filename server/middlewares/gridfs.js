8c3db6a47e651f73483cb7c4f7240ce91b484578
Naman Vyas <dellLaptop@naman.com>
2022-11-05 18:21:56 +0530

    correction

--- server/middlewares/gridfs.js
+++ server/middlewares/gridfs.js
@@ -38,31 +38,31 @@ async function sendFile(req, res, next) {
   const filePath = path.join(DOWNLOAD_PATH, imgId.toString() + ".jpeg")
 
   const gridfsbucket = getGridFsBucket()
-  gridfsbucket
-    .openDownloadStream(imgId)
-    .pipe(fs.createWriteStream(filePath))
-    .on("error", () => {
-      console.log("Some error occurred in download:" + error);
-      next()
-    })
-    .on("finish", () => {
-      console.log("Done downloading!!");
-      res.sendFile(filePath, () => {
-        deleteFile(filePath)
-      })
-      next()
-    })
   // gridfsbucket
   //   .openDownloadStream(imgId)
-  //   .pipe(res)
+  //   .pipe(fs.createWriteStream(filePath))
   //   .on("error", () => {
   //     console.log("Some error occurred in download:" + error);
   //     next()
   //   })
   //   .on("finish", () => {
-  //     console.log("File sent successfully!!");
+  //     console.log("Done downloading!!");
+  //     res.sendFile(filePath, () => {
+  //       deleteFile(filePath)
+  //     })
   //     next()
   //   })
+  gridfsbucket
+    .openDownloadStream(imgId)
+    .pipe(res)
+    .on("error", () => {
+      console.log("Some error occurred in download:" + error);
+      next()
+    })
+    .on("finish", () => {
+      console.log("File sent successfully!!");
+      next()
+    })
 }
 
 
